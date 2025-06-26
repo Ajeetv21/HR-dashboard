7bbdabd2c19c9874a2510bf4ad6811584c182a7a
Ajeetv21 <ajeetv628@gmail.com>
2025-06-26 16:30:03 +0530

    git err2

--- backend/app.js
+++ backend/app.js
@@ -17,10 +17,7 @@ const PORT = 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors());
 connectDB();
 
 app.use("/files", express.static("files"));
