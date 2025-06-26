aff79cbef96851991cdde95972ee9d92316d6daa
Ajeetv21 <ajeetv628@gmail.com>
2025-06-26 19:30:32 +0530

    mongo

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,18 @@
-const mongoose = require("mongoose");
-
+const mongoose = require("mongoose")
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      serverSelectionTimeoutMS: 5000, // Optional: Useful for dev/debug
-    });
+    try {
 
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`❌ MongoDB connection error: ${error.message}`);
-    process.exit(1); // Exit process with failure
-  }
-};
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
+        });
+        console.log("db connection");
+
+    } catch (error) {
+        console.log(error.message)
+
+    }
+}
 
 module.exports = connectDB;
